538ebf2dbb57d9a906c9dc5c9a2508a4f6119b5a
Morgan Stanley <morganpstanley@gmail.com>
2019-07-03 20:52:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    last = array.pop
+    array_string = array.join (', ')
+    array_string << ", and "
+    array_string << last
+  end
 end
